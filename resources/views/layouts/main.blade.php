4c6b3f739092f0c0be7a42b8758076e9569bdc0a
rizkysafdila <rizkysafdila@gmail.com>
2022-07-11 14:51:04 +0700

    init project

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Personal Nutritionist</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  {{-- <script src="https://platform.fatsecret.com/js?key=d446e4fd8ab647c0831a0068b9a3193c"></script> --}}
+</head>
+
+<body>
+
+  <div style="width:500px;margin:0 auto;">
+    <script src="https://platform.fatsecret.com/js?key=d446e4fd8ab647c0831a0068b9a3193c&amp;auto_load=true"></script>
+  </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</body>
+
+</html>
