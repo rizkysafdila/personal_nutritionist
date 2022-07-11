61ae37125858a1ed2c686a3f23cb7b9eb5a8e439
rizkysafdila <rizkysafdila@gmail.com>
2022-07-11 15:00:51 +0700

    some changes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Food;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,11 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Food::factory()->create([
+            'name' => '',
+            'weight' => '',
+            'calories' => '',
+        ]);
     }
 }
